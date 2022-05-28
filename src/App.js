bae6666c13aca9657ebbfcfbda64c8ab25ebda9d
MAHossain1 <mahpro110@gmail.com>
2022-05-28 08:14:21 +0600

    requireAuth implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blog from "./pages/Blog/Blog";
 import Home from "./pages/Home/Home";
+import RequireAuth from "./pages/Login/RequireAuth";
 import SignIn from "./pages/Login/SignIn";
 import SignUp from "./pages/Login/SignUp";
 import Navbar from "./pages/Shared/Navbar";
@@ -11,6 +13,15 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/blog"
+          element={
+            <RequireAuth>
+              <Blog />
+            </RequireAuth>
+          }
+        />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h2 className="text-center text-4xl text-primary">
+        Welcome to GymEqu Blog!!
+      </h2>
+    </div>
+  );
+};
+
+export default Blog;
